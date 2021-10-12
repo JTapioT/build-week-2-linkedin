2bd485fffb103b9c7d777cd5c7fcfaa7a9bf4590
JTapioT <88656150+JTapioT@users.noreply.github.com>
2021-10-12 13:41:01 +0200

    Update repository
    Updates to Experience component. Also, separate component, UserExperienceDetail was needed for Experience component when populating individual experiences.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,21 @@
 import {useState, useEffect} from "react";
+import UserExperienceDetails from "./UserExperienceDetails";
 
 function Experience(props) {
 
-    const [user, setUser] = useState({});
+  /*  const [user, setUser] = useState({}); */
     const [userId, setUserId] = useState(props.id);
+    const [userExperience, setUserExperience] = useState([]);
+    const [isExperienceIconHover, setExperienceIconHover] = useState(false);
 
-
-    console.log(userId);
-
+    const experienceIconStyle = {
+      fontSize: "1.4em",
+      backgroundColor: isExperienceIconHover ? "lightgrey" : null,
+      borderRadius: "50%",
+      padding: "0.1em",
+      cursor: isExperienceIconHover ? "pointer" : null,
+    };
+    
     async function fetchUser() {
       try {
         let response = await fetch(
@@ -21,8 +29,9 @@ function Experience(props) {
         );
 
         if(response.ok) {
-          let responseJson = await response.json();
-          console.log(responseJson);
+          let responseJSON = await response.json();
+          console.log(responseJSON);
+          setUserExperience(responseJSON);
         }
       } catch(error) {
         console.log(error);
@@ -36,7 +45,8 @@ function Experience(props) {
 
 
   return (
-    <div className="mt-3"
+    <div
+      className="mt-3"
       style={{
         borderTopRightRadius: "18px",
         borderTopLeftRadius: "18px",
@@ -49,84 +59,49 @@ function Experience(props) {
     >
       <div className="d-flex justify-content-between align-items-center">
         <h5>Experience</h5>
-        <i class="bi bi-plus-lg" style={{ fontSize: "20px" }}></i>
+        <i
+          class="bi bi-plus-lg"
+          style={experienceIconStyle}
+          onMouseEnter={() => {
+            setExperienceIconHover(true);
+          }}
+          onMouseLeave={() => {
+            setExperienceIconHover(false);
+          }}
+          onClick={() => {
+            props.history.push("/edit/forms/position/new");
+          }}
+        ></i>
       </div>
       <div className="d-flex flex-column mt-3">
-        {/* Occupation details */}
-        <div className="d-flex">
-          <div className="mr-3">
-            <img
-              src={
-                "http://www.floorepoxyindustrial.com/images/colorchart/medium-gray.jpg"
-              }
-              style={{ width: "80px", height: "80px" }}
-              alt="thumbnail for now"
-            />
-          </div>
-
-          <div
-            className="d-flex justify-content-between w-100"
-            style={{ borderBottom: "0.5px solid #ccc", paddingBottom: "1em" }}
-          >
-            <div className="d-flex flex-column justify-content-between">
-              <h5 className="m-0">Occupation</h5>
-              <p className="m-0">Company</p>
-              <small className="m-0">Oct 2021 - Oct 2021</small>
+        {userExperience.length ? (
+          userExperience.map((experience) => {
+            return <UserExperienceDetails experience={experience} />;
+          })
+        ) : (
+          <div className="d-flex">
+            <div className="mr-3">
+              <img
+                src={
+                  "http://www.floorepoxyindustrial.com/images/colorchart/medium-gray.jpg"
+                }
+                style={{ width: "80px", height: "80px" }}
+                alt="thumbnail for now"
+              />
             </div>
-            <i class="bi bi-pencil" style={{ fontSize: "20px" }}></i>
-          </div>
-        </div>
-        {/* Occupation detail row ends */}
-        {/* Occupation details */}
-        <div className="d-flex mt-4">
-          <div className="mr-3">
-            <img
-              src={
-                "http://www.floorepoxyindustrial.com/images/colorchart/medium-gray.jpg"
-              }
-              style={{ width: "80px", height: "80px" }}
-              alt="thumbnail for now"
-            />
-          </div>
-
-          <div
-            className="d-flex justify-content-between w-100"
-            style={{ borderBottom: "0.5px solid #ccc", paddingBottom: "1em" }}
-          >
-            <div className="d-flex flex-column justify-content-between">
-              <h5 className="m-0">Occupation</h5>
-              <p className="m-0">Company</p>
-              <small className="m-0">Oct 2021 - Oct 2021</small>
+            <div
+              className="d-flex justify-content-between w-100"
+              style={{ borderBottom: "0.5px solid #ccc", paddingBottom: "1em" }}
+            >
+              <div className="d-flex flex-column justify-content-between">
+                <h5 className="m-0">Occupation</h5>
+                <p className="m-0">Company</p>
+                <small className="m-0">Oct 2021 - Oct 2021</small>
+              </div>
+              <i className="bi bi-pencil" style={{ fontSize: "20px" }}></i>
             </div>
-            {/* <i class="bi bi-pencil" style={{ fontSize: "20px" }}></i> */}
           </div>
-        </div>
-        {/* Occupation detail row ends */}
-        {/* Occupation details */}
-        <div className="d-flex mt-4">
-          <div className="mr-3">
-            <img
-              src={
-                "http://www.floorepoxyindustrial.com/images/colorchart/medium-gray.jpg"
-              }
-              style={{ width: "80px", height: "80px" }}
-              alt="thumbnail for now"
-            />
-          </div>
-
-          <div
-            className="d-flex justify-content-between w-100"
-            style={{ borderBottom: "0.5px solid #ccc", paddingBottom: "1em" }}
-          >
-            <div className="d-flex flex-column justify-content-between">
-              <h5 className="m-0">Occupation</h5>
-              <p className="m-0">Company</p>
-              <small className="m-0">Oct 2021 - Oct 2021</small>
-            </div>
-            {/* <i class="bi bi-pencil" style={{ fontSize: "20px" }}></i> */}
-          </div>
-        </div>
-        {/* Occupation detail row ends */}
+        )}
       </div>
     </div>
   );
