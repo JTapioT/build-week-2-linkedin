98cc63e35a5af6e8e8dd2ce735d5b9ae48606ea3
JTapioT <88656150+JTapioT@users.noreply.github.com>
2021-10-14 09:36:28 +0200

    Update App.js
    Route was wrong. Now fixed.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <>
       <BrowserRouter>
         <Route
-          path="/profile/:id"
+          path="/feed"
           render={(props) => (
             <>
               <MyNavBar />
